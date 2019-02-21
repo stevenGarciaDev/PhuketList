0a62fd43fac96a9b06e4dc7e89c68409ce9d3d50
stevenGarciaDev <stevgarcia2015@gmail.com>
2019-02-21 14:44:52 -0800

    Clean markup by removing inline styles

--- front-end/src/pages/MyBucketList.jsx
+++ front-end/src/pages/MyBucketList.jsx
@@ -8,16 +8,17 @@ class MyBucketList extends Component {
     return (
       <div>
         <Navbar />
-        <div className="jumbotron bucket-list-jumbotron text-center" style={ { backgroundColor: "#fff" } }>
+
+        <div className="jumbotron text-center" id="bucket-list-jumbotron">
           <h1 className="page-title">My Bucket List</h1>
           <h2 className="sub-header">What have you always wanted to do?</h2>
 
-          <div className="input-group col-md-6 col-md-offset-3" style={ { margin: '0 auto' }}>
+          <div className="input-group col-md-6 col-md-offset-3">
             <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" />
             <button className="btn btn-outline-success" type="submit">Add New Task</button>
           </div>
-
         </div>
+
         <BucketList />
       </div>
     );
