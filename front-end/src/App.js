4fe9d2c2a3bcd0d5c3515a72cb378d43c84e650a
Kratos <repinosa@gmail.com>
2019-02-24 14:25:36 -0800

    Installed bootstrap

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
+
+// Pages
 import MyBucketList from './pages/MyBucketList';
+
+// Components
+import Navbar from './components/Navbar';
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 
