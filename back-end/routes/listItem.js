d2d5ff92f48038a98c0da83cac5848c4e9fe0dd3
stevenGarciaDev <stevgarcia2015@gmail.com>
2019-03-02 13:20:58 -0800

    Add background image for register and login components.

--- back-end/routes/listItem.js
+++ back-end/routes/listItem.js
@@ -3,45 +3,30 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
-
-router.get("/", async (req, res) => {
-  const listItems = await ListItem.find();
+// Get all list items
+router.get('/', async (req, res) => {
+  const listItems = await ListItem.find().select('taskName');
   res.send(listItems);
 });
 
-router.post("/", async (req, res) => {
-  const { error } = validateItem(req.body);
+// Get a particular list item
+router.get('/:id', async (req, res) => {
+  const listItem = await ListItem.findById(req.params.id);
+  res.send(listItem);
+});
+
+router.post('/', (req, res) => {
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-
-  let listItem = new ListItem({
-    taskName: req.body.taskName
-  });
-
-  listItem = await listItem.save();
-
-  res.send(listItem);
 });
 
-router.put("/", async (req, res) => {
-  const { error } = validateItem(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.put('/:id', (req, res) => {
 
-  let listItem = ListItem.findByIdAndUpdate( req.params.id, {
-    taskName: req.body.taskName,
-    dateModified: Date.now
-  }, { new: true });
-
-  if (!listItem) return res.status(404).send('The list item with the given ID was not found.');
-
-  res.send(listItem);
 });
 
-router.delete("/", async (req, res) => {
-  const listItem = await ListItem.findByIdAndRemove( req.params.id );
+router.delete('/:id', (req, res) => {
 
-  if (!listItem) return res.status(404).send('The list item with the given ID was not found.');
-
-  res.send(listItem);
 });
+
 
 module.exports = router;
