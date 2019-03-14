eae1b42c485d5fe7f5b1c3cf4c34fd4247338104
stevenGarciaDev <stevgarcia2015@gmail.com>
2019-03-14 16:12:41 -0700

    Improve front end activity feed and post by making it responsive.

--- front-end/src/components/post.jsx
+++ front-end/src/components/post.jsx
@@ -3,6 +3,19 @@ import photo from '../assets/images/jackie-tsang-458443-unsplash.jpg';
 
 const Post = props => {
   return (
+    <React.Fragment>
+    <div className="Post">
+      <img className="post-profile-img" />
+      <h1 className="post-author">John Smith</h1>
+      <h2 className="post-date">30 min</h2>
+      <p className="post-content">
+        Donec id elit non mi porta gravida at eget metus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+      </p>
+
+      <img className="post-img" src={photo} alt="Post" />
+
+    </div>
     <div className="Post">
       <img className="post-profile-img" />
       <h1 className="post-author">John Smith</h1>
@@ -13,6 +26,7 @@ const Post = props => {
 
       <img className="post-img" src={photo} alt="Post" />
     </div>
+    </React.Fragment>
   );
 };
 
